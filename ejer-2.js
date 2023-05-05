1123f40e7699099143c38ef47379d33b776bb0fe
zetaberry <126761064+zetaberry@users.noreply.github.com>
2023-05-05 12:31:23 +0200

    Add files via upload

---
+++ ejer-2.js
@@ -0,0 +1,29 @@
+const fechaCumple = "2 de septiembre de 1993";
+
+
+// Divide la fecha en sus componentes (día, mes, año) y agrégala a la lista
+
+const dia = "2";
+const mes = "septiembre";
+const anio = "1993";
+//const [dia, mes, anio] = fechaCumple(" de ");
+
+
+console.log(fechaCumple);
+
+const libroFavorito = {
+    titulo: "El guardian entre el centeno",
+    tamaño: "bolsillo",
+    numeroPaginas: 288,
+    editorial: "Alianza",
+    isEspaniol: false,
+    URL: 'https://www.casadellibro.com/libro-el-guardian-entre-el-centeno/9788420674209/1798932',
+}
+
+
+
+
+
+const lista = ["zahira", 29, false, fechaCumple, libroFavorito];
+
+
